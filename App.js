39243a053700a5fed8cbfca675f61424bb71c78d
liu <liuwei811007@gmail.com>
2020-06-20 16:03:04 +0800

    init count game

--- App.js
+++ App.js
@@ -1,97 +1,118 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import { useState } from 'react';
+import { AppLoading } from 'expo';
+import { useFonts } from '@use-expo/font';
 
 export default function App() {
-  const [phoneNumber, setPhoneNumber] = useState('')
-  const [ansMsg, setAnsMsg] = useState('')
-  const checkPhoneNumber = () => {
-    if (phoneNumber.length <= 9) {
-      setAnsMsg("電話號碼太短了啦!")
-    } else if (phoneNumber.substr(0, 2) != '09') {
-      setAnsMsg("手機開頭不是09?整我喔")
-    } else {
-      setAnsMsg("恭喜您!從你的手機號碼知道,您剛剛浪費了此生最沒有意義的1分鐘")
-    }
-  }
-  return (
-    <View style={styles.container}>
-      <Text style={styles.titleText}>手機妙算</Text>
-      <View style={styles.subTitleGroup}>
-        <Text style={styles.subTitle}>手機號碼與您的命運有著密不可分的關係,請輸入您的手機號碼,解析您的命運!(胡說八道請不要當真)</Text>
-        <Text>{ansMsg}</Text>
-      </View>
+  const [countNumber, setCountNumber] = useState(0)
+  const lion = require('./src/img/lion2.png')
+  const wolf = require('./src/img/wolf2.png')
+  const dragon = require('./src/img/dragon2.png')
+  const groupArr = [{ name: 'lion', color: "#27496d", image: lion }, { name: 'wolf', color: "orange", image: wolf }, { name: '3b6978', color: "#3b6978", image: dragon }]
+  var [nowGroupNumber, setNowGroupNumber] = useState(0)
+  var [nowGroup, setNowGroup] = useState(groupArr[nowGroupNumber])
+  let [fontsLoaded] = useFonts({
+    'GameOfThrones': require('./src/font/GameOfThrones.otf')
+  })
 
-      <View style={styles.inputGroup}>
-        <Text style={styles.inputLabel}>手機號碼:</Text>
-        <TextInput
-          style={styles.inputText}
-          placeholder="請輸入您的手機號碼"
-          onChangeText={(text) => {
-            setPhoneNumber(text)
-          }}
-          maxLength={10}
-          keyboardType={'numeric'}
-        ></TextInput>
+
+  const changeGroup = () => {
+    let tempNumber = nowGroupNumber + 1
+    if (tempNumber < 0) tempNumber = tempNumber + 3
+    tempNumber = tempNumber % 3
+    setNowGroupNumber(tempNumber)
+    setNowGroup(groupArr[tempNumber])
+
+  }
+  const addNumber = () => {
+    setCountNumber(countNumber + 1)
+  }
+  const reduceNumber = () => {
+    setCountNumber(countNumber - 1)
+  }
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  else {
+    return (
+      <View style={styles.container}>
+        <Text style={[styles.title, { color: nowGroup.color }]}>Game Of Count</Text>
+        <View style={styles.logoGroup}>
+          <Image style={styles.logo} source={nowGroup.image}></Image>
+          <Text style={styles.countText}>{countNumber}</Text>
+        </View>
+        <View style={styles.countBtnGroup}>
+          <TouchableOpacity
+            style={[styles.btnStyle, { backgroundColor: nowGroup.color, margin: 10 }]}
+            onPress={() => { addNumber() }}
+            color="#8ac6d1"
+          >
+            <Text style={{ color: '#fffdf9', fontFamily: "GameOfThrones" }}>
+              ADD
+          </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.btnStyle, { backgroundColor: nowGroup.color, margin: 10 }]}
+            onPress={() => { reduceNumber() }}
+            color="#8ac6d1"
+          >
+            <Text style={{ color: '#fffdf9', fontFamily: "GameOfThrones" }}>
+              sub
+          </Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+          style={[styles.btnStyle, { backgroundColor: nowGroup.color }]}
+          onPress={() => { changeGroup() }}
+          color="#8ac6d1"
+        >
+          <Text style={{ color: '#fffdf9' }}>
+            切換陣營
+          </Text>
+        </TouchableOpacity>
       </View>
-      <Button
-        style={styles.btnStyle}
-        title="了解您的命運"
-        onPress={() => { checkPhoneNumber() }}
-        color="#8ac6d1"
-      >
-      </Button>
-    </View>
-  );
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#beebe9',
+    backgroundColor: '#fffdf9',
     alignItems: 'center',
     justifyContent: 'center',
     width: '100%'
   },
-  mainText: {
-    color: 'blue',
-    fontSize: 50
+  title: {
+    fontFamily: "GameOfThrones",
+    fontSize: 35
+  },
+  logo: {
+    width: "100%",
+    height: "100%",
 
   },
-  subTitleGroup: {
-    width: '80%'
+  logoGroup: {
+    margin: 50,
+    width: 300,
+    height: 300
   },
-  titleText: {
-    fontSize: 50,
-    color: '#8ac6d1',
-    textShadowColor: '#3b6978',
-    textShadowOffset: { width: -1, height: 1 },
-    textShadowRadius: 5
-  },
-  subTitle: {
-    textAlign: 'left',
-    margin: 15,
-    color: "#3b6978"
-  },
-  inputGroup: {
-    width: '80%',
-    alignItems: 'center',
-    flexDirection: "row"
-  },
-  inputText: {
-    backgroundColor: "#fffdf9",
-    width: '80%',
-    height: 50,
-    fontSize: 20,
+  countText: {
+
     textAlign: "center",
-    borderColor: "#8ac6d1",
-    borderWidth: 2,
-    borderRadius: 2
-  },
-  inputLabel: {
-    color: '#8ac6d1'
+
+
   },
   btnStyle: {
-    backgroundColor: '#ffe3ed'
+    backgroundColor: '#ffe3ed',
+    padding: 15,
+    borderRadius: 5
+
+  },
+  countBtnGroup: {
+    flexDirection: "row"
   }
 });
